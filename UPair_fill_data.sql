86cb1778eb8106cc0b0ab2f8bd055cff19f7e92e
pmc44 <46945240+pmc44@users.noreply.github.com>
2019-03-05 00:02:13 -0600

    Filled in test data to database

---
+++ UPair_fill_data.sql
@@ -0,0 +1,54 @@
+use UPair;
+sELECT * from SCHOOL
+
+INSERT INTO INSTRUCTOR
+VALUES(10, 228459, 'Benjamin', 'Franklin', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(11, 228459, 'John', 'Kennedy', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(12, 228459, 'George', 'Washington', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(13, 228459, 'Isaac', 'Newton', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(14, 228459, 'Marie', 'Curie', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(15, 228459, 'Charles', 'Darwin', 'Dr.');
+INSERT INTO INSTRUCTOR
+VALUES(16, 228459, 'John', 'Adams', 'Dr.');
+
+INSERT INTO STUDENT
+VALUES('A00000001', 'joe@gmail.com', 'Joseph', 'Smith', 228459);
+INSERT INTO STUDENT
+VALUES('A00000002', 'bob@gmail.com', 'Robert', 'Smith', 228459);
+INSERT INTO STUDENT
+VALUES('A00000003', 'mike@gmail.com', 'Michael', 'Scott', 228459);
+INSERT INTO STUDENT
+VALUES('A00000004', 'scott@gmail.com', 'Scott', 'Rogers', 228459);
+INSERT INTO STUDENT
+VALUES('A00000005', 'jack@gmail.com', 'Jackson', 'Sterling', 228459);
+INSERT INTO STUDENT
+VALUES('A00000006', 'sally@gmail.com', 'Sally', 'Peters', 228459);
+INSERT INTO STUDENT
+VALUES('A00000007', 'sarah@gmail.com', 'Sarah', 'Waters', 228459);
+INSERT INTO STUDENT
+VALUES('A00000008', 'sue@gmail.com', 'Susan', 'Anthony', 228459);
+INSERT INTO STUDENT
+VALUES('A00000009', 'kelly@gmail.com', 'Kelsie', 'Queen', 228459);
+INSERT INTO STUDENT
+VALUES('A00000010', 'kate@gmail.com', 'Katherine', 'Bell', 228459);
+
+
+INSERT INTO CLASS
+VALUES('2038', 228459, 10, '3', 'Computer Science', 'CS Building', '238', 0);
+INSERT INTO CLASS
+VALUES('2048', 228459, 11, '2', 'Computer Science', 'CS Building',  '238', 0);
+INSERT INTO CLASS
+VALUES('3096', 228459, 14, '1', 'Chemistry', 'Chemistry Building', '1',  0);
+INSERT INTO CLASS
+VALUES('4046', 228459, 16, '251', 'Computer Science', '114A', 'CS Building', 0);
+INSERT INTO CLASS
+VALUES('2058', 228459, 12, '1', 'Computer Science', '114B', 'CS Building', 0);
+INSERT INTO CLASS
+VALUES('3081', 228459, 15, '1', 'Biology', '114B', 'Biology Building', 0);
+INSERT INTO CLASS
+VALUES('1015', 228459, 13, '1', 'Physics', '114B', 'Physics Building', 0);
