020547b4b50c1d7b8653b7d1d47ac00e180d0b54
Cavcode <cameronav23@gmail.com>
2019-03-11 22:35:47 -0500

    Class where database methods will be defined.

--- se_app/DatabaseCalls.py
+++ se_app/DatabaseCalls.py
@@ -5,7 +5,20 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM dbo.STUDENT')
+cursor.execute('SELECT StudentID FROM dbo.StudentClass')
+
+#Needs to be translated into a pyodbc sql statement.
+#WHERE COURSEID in
+
+    #(SELECT COURSEID
+
+    #from STUDENTCLASS
+
+    #WHERE STUDENTID = 'A00000002')
+
+#GROUP BY STUDENTID
+
+#HAVING COUNT(STUDENTID) >= 2;
 
 for row in cursor:
     print(row)
