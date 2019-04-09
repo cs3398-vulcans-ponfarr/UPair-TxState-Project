96ea6509edabd29c10034c54e83614a3931625cb
Cameron Valdez <cameronav23@gmail.com>
2019-04-09 16:10:25 -0500

    Updated databasecalls.py with Peter Update
    Peter fixed the syntax of the database calls to SQL. Updated the DatabaseCalls.py file to reflect those changes.

--- se_app/DatabaseCalls.py
+++ se_app/DatabaseCalls.py
@@ -5,23 +5,30 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT StudentID FROM dbo.StudentClass')
-
-#Needs to be translated into a pyodbc sql statement.
-#WHERE COURSEID in
-
-    #(SELECT COURSEID
-
-    #from STUDENTCLASS
-
-    #WHERE STUDENTID = 'A00000002')
-
-#GROUP BY STUDENTID
-
-#HAVING COUNT(STUDENTID) >= 2;
-
-
-
+cursor.execute('SELECT * FROM dbo.STUDENT')
 
 for row in cursor:
     print(row)
+
+def getMessage(user, target_user, school_id):
+    message = cursor.execute("""
+    select MessageText
+      from AUTO_MESSAGES
+     where SchoolID = ?
+       and MessageID = 1
+    """, school_id)
+    to_data = cursor.execute("""
+    select FirstName +' '+LastName as Name, Email
+      from STUDENT
+     where StudentID = ?
+     and SchoolID = ?
+    """, user,school_id).fetchone()
+    from_data = cursor.execute("""
+    select FirstName +' '+LastName as Name, Email
+      from STUDENT
+     where StudentID = ?
+     and SchoolID = ?
+    """, target_user,school_id).fetchone()
+    message.format(user=to_data.Name, target_user=from_data.Name, \
+                   target_user_email=from_data.Email)
+return message
