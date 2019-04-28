01bf41bca6d6e242b39fa940db95fb09a3c59e1f
Peter Cowsar <Peter Cowsar@DESKTOP-5E19TC4>
2019-04-28 18:23:36 -0500

    changes
    changes

--- se_app/app.py
+++ se_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+import DatabaseCalls
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bc158abb4eeeedac31b250d17e4f4bae'
@@ -20,17 +21,33 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        if DatabaseCalls.Register(form.school.data, form.email.data, form.student_id.data, form.password.data):
+            flash(f'Account created for {form.email.data}!', 'register')
+        else:
+            flash(f'Email {form.email.data} already taken!', 'register')
+        #return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/schedule", methods=['GET', 'POST'])
+def schedule():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if DatabaseCalls.Register(form.school.data, form.email.data, form.student_id.data, form.password.data):
+            flash(f'Account created for {form.email.data}!', 'register')
+        else:
+            flash(f'Email {form.email.data} already taken!', 'register')
+        #return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    #confirm = DatabaseCalls()
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        if form.email.data != '' and form.password.data != '':
+            flash(DatabaseCalls.getAccount(form.email.data, form.password.data), 'success')
+            #if confirm.getAccount()
             #{{form.email(class_="flagged")}}
             #return redirect(url_for('home'))
         else:
